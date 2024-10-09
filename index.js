4baeab37d397c5a3ac1ce57c63ad02b44b259f29
Billy Wong <bwong.yh@gmail.com>
2024-10-09 15:03:02 -0600

    add styling

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 form = document.querySelector('.signup-form');
 feedback = document.querySelector('.feedback');
 
+const pattern = /^\w{6,}$/;
+
 form.addEventListener('submit', e => {
   // prevent flashing when submitting the form
   e.preventDefault();
 
-  const pattern = /^\w{6,}$/;
   const username = form.username.value;
 
+  feedback.removeAttribute('class');
   feedback.style.display = 'block';
 
   if (!pattern.test(username)) {
+    feedback.style.color = 'red';
     feedback.innerText =
       'username must contain letters and numbers only & at least 6 characters long';
     return;
   }
 
+  feedback.style.color = 'green';
   feedback.innerText = 'username is valid';
+});
+
+// keyup event for live feedback
+form.username.addEventListener('keyup', e => {
+  if (!pattern.test(e.target.value)) {
+    e.target.setAttribute('class', 'error');
+  } else {
+    e.target.setAttribute('class', 'success');
+  }
 });
